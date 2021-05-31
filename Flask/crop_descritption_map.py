ff0eed4dfa5e56048cf13e9a0d49d6c2b3de7bfd
satishjasthi <satish27may@gmail.com>
2021-05-31 23:11:47 +0530

    add requirements.txt and update crop description map

---
+++ Flask/crop_descritption_map.py
@@ -0,0 +1,112 @@
+cd = {
+    "rice": {
+        "crop_dis": "In India, West Bengal is the largest rice producing state. Paddy fields are a common sight throughout India, both in the northern Gangetic Plains and the southern peninsular plateaus. Paddy is cultivated at least twice a year in most parts of India, the two seasons being known as Rabi and Kharif respectively.India is the second largest producing country in the world.",
+        "crop_link": "  https://en.wikipedia.org/wiki/Rice",
+        "crop_img": "/static/styles/images/rice.jpg",
+    },
+    "maize": {
+        "crop_dis": "Maize/Corn is largely cultivated crop in north India. Major Maize or Corn growing states are Uttar Pradesh, Rajasthan, MP, Bihar, Himachal Pradesh, Jammu and Kashmir, and Punjab which together account for two-thirds of the total area and output of the crop.",
+        "crop_link": "https://en.wikipedia.org/wiki/Maize",
+        "crop_img": "/static/styles/images/maize.jpg",
+    },
+    "chickpea": {
+        "crop_dis": "In India, chick pea producing states are Madhya Pradesh, Uttar Pradesh, Rajasthan, Maharashtra and Andhra Pradesh. Madhya Pradesh has major share i.e. around 40 per cent in the India's chick pea production.",
+        "crop_link": "https://en.wikipedia.org/wiki/Chickpea",
+        "crop_img": "/static/styles/images/chikpea.jpg",
+    },
+    "kidneybeans": {
+        "crop_dis": "Major Red Kidney Beans Grown States in India are Maharashtra, Jammu and Kashmir, Himachal Pradesh, Uttarakhand ,West Bengal, Uttar Pradesh ,Tamil Nadu, Kerala and Karnataka. This crop grows well in tropical and temperate areas receiving 60 to 150 cm of rainfall annually.",
+        "crop_link": "https://en.wikipedia.org/wiki/Rajma",
+        "crop_img": "/static/styles/images/kideybeans.jpg",
+    },
+    "pigeonpeas": {
+        "crop_dis": "Highest production of Pigeonpea is from Maharashtra which is around 30% of National Production. More than 90% of production contribution of Tur is from 8 states, namely, Maharashtra, Karnataka, Madhya Pradesh, Uttar Pradesh, Gujarat, Jharkhand, Telangana and Andhra Pradesh.",
+        "crop_link": "https://en.wikipedia.org/wiki/Pigeon_pea",
+        "crop_img": "/static/styles/images/pigonpea.jpg",
+    },
+    "mothbeans": {
+        "crop_dis": "Area and production of moth bean has been highest in Rajasthan (96.75% and 94.49%) followed by Gujarat (2.38% and 3.6%). However, yield of Rajasthan (292 kg/ha) was below the National average yield of (299 kg/ha). It can tolerate high temperature without any adverse effect on flowering and fruit development.",
+        "crop_link": "https://en.wikipedia.org/wiki/Vigna_aconitifolia",
+        "crop_img": "/static/styles/images/monthbeans.jpg",
+    },
+    "mungbean": {
+        "crop_dis": "In India, mungbean is grown on an area of about 3 million hectares with the production of about 1 million tonnes. The major mungbean grown states are Orissa, Maharashtra, Andhra Pradesh, Telangana, Rajasthan, Madhya Pradesh, Bihar, Karnataka, and Uttar Pradesh.",
+        "crop_link": "https://en.wikipedia.org/wiki/Mung_bean",
+        "crop_img": "/static/styles/images/mungbeans.jpg",
+    },
+    "blackgram": {
+        "crop_dis": "Black gram is a highly priced pulse, very rich in phosphoric acid. ... The states of Madhya Pradesh (16.50 lakh ha), Uttar Pradesh (7.01 lakh ha), Rajasthan (4.56 lakh ha), Maharashtra (2.87 lakh ha), Karnataka (0.687 lakh ha) and Andhra Pradesh (0.11 lakh ha) are the major producers of black gram in India during Kharif.",
+        "crop_link": "https://en.wikipedia.org/wiki/Vigna_mungo",
+        "crop_img": "/static/styles/images/blackgram.png",
+    },
+    "lentil": {
+        "crop_dis": "In 2018, global production of lentils was 6.3 million tonnes, led by Canada with 33% and India with 25% of the world total (table). ... In India, Madhya Pradesh and Uttar Pradesh are largest producers with both producing more than 70 percent of the total. Other major producers include West Bengal and Bihar.",
+        "crop_link": "https://en.wikipedia.org/wiki/Lentil",
+        "crop_img": "/static/styles/images/lentil.jpg",
+    },
+    "pomegranate": {
+        "crop_dis": "In India, major pomegranate producing states are Maharashtra, Karnataka, Gujarat, Andhra Pradesh, Madhya Pradesh, Tamil Nadu Rajasthan. Maharashtra leads with 90 thousand ha areas with an annual production of 9.45 lakh Mt tones and 10.5 Mt/ha productivity.",
+        "crop_link": "https://en.wikipedia.org/wiki/Pomegranate",
+        "crop_img": "/static/styles/images/pomogranate.jpg",
+    },
+    "banana": {
+        "crop_dis": "Banana and plantains are grown in about 120 countries. Total annual world production is estimated at 86 million tonnes of fruits. India leads the world in banana production with an annual output of about 14.2 million tonnes.",
+        "crop_link": "https://en.wikipedia.org/wiki/Banana",
+        "crop_img": "/static/styles/images/banana.jpg",
+    },
+    "mango": {
+        "crop_dis": "The major mango-growing states are Andhra Pradesh, Uttar Pradesh, Karnataka, Bihar, Gujarat and Tamil Nadu. Uttar Pradesh ranks first in mango production with a share of 23.47 % and highest productivity.",
+        "crop_link": "https://en.wikipedia.org/wiki/Mango",
+        "crop_img": "/static/styles/images/mango.jpg",
+    },
+    "grapes": {
+        "crop_dis": "Maharashtra. Maharashtra is the India's largest producer of grapes and Nashik also know as Wine Capital of India, leads the list of grape producing areas in Maharashtra followed by Sangli, Satara ,Ahmednagar and Karnataka.",
+        "crop_link": "https://en.wikipedia.org/wiki/Grape",
+        "crop_img": "/static/styles/images/",
+    },
+    "watermelon": {
+        "crop_dis": "top watermelon producing states in India includes Uttar Pradesh, Andra Pradesh, Karnataka, West Bengal, Orissa, Tamil Nadu, Madhya Pradesh, Haryana, Telangana, Maharashtra. From which southern states like Tamil Nadu, Maharashtra, Andra Pradesh cultivate watermelons all year long",
+        "crop_link": "https://en.wikipedia.org/wiki/Watermelon",
+        "crop_img": "/static/styles/images/watermelon.jpg",
+    },
+    "muskmelon": {
+        "crop_dis": "In India, it's basically cultivated as vegetable predominately in Uttar Pradesh, Andhra Pradesh, Punjab, Madhya Pradesh and Karnataka during summer and rainy months.",
+        "crop_link": "https://en.wikipedia.org/wiki/Cucumis_melo",
+        "crop_img": "/static/styles/images/muskmelon.jpg",
+    },
+    "apple": {
+        "crop_dis": "In India, Apple is primarily cultivated in Jammu & Kashmir; Himachal Pradesh; hills of Uttar Pradesh and Uttaranchal. It is also cultivated to a small extent in Arunachal Pradesh; Nagaland; Punjab and Sikkim.",
+        "crop_link": "https://en.wikipedia.org/wiki/Apple",
+        "crop_img": "/static/styles/images/apple.jpg",
+    },
+    "orange": {
+        "crop_dis": "Maharashtra is the leading orange (Mandarin) producing State with 8.27 lakh tonnes (2009-10) accounting for 40 per cent of total production and yield of 6 tonnes a hectare. Madhya Pradesh ranks second with production of 6.78 lakh tonnes followed by Assam (1.42 lakh tonnes).",
+        "crop_link": "https://en.wikipedia.org/wiki/Orange",
+        "crop_img": "/static/styles/images/orange.jpg",
+    },
+    "papaya": {
+        "crop_dis": "The papaya plant has short life, hence the area under cultivation varies greatly in different years. In India it is cultivated over an area of 97.7 thousand hectares with annual production of 3628.9 thousand MT. In Punjab it is being grown as filler plant in mango and litchi orchards.",
+        "crop_link": "https://en.wikipedia.org/wiki/Papaya",
+        "crop_img": "/static/styles/images/papaya.jpg",
+    },
+    "coconut": {
+        "crop_dis": "Traditional areas of coconut cultivation are the states of Kerala, Karnataka, Andhra Pradesh and Tamil Nadu. Also Maharashtra, Odisha, West Bengal, Gujarat, Puducherry and Goa; and the island territories of Lakshadweep and Andaman and Nicobar are other areas of coconut production.",
+        "crop_link": "https://en.wikipedia.org/wiki/Coconut",
+        "crop_img": "/static/styles/images/coconut.jpg",
+    },
+    "cotton": {
+        "crop_dis": "Maharashtra is the largest producer of cotton in India and produced 82 lakh bales of cotton in India. The cotton production covers 42.54 lakh hectares in Maharashtra. The largest cotton-producing regions of Maharashtra are Yavatamal, Vidarbha, Khandesh, Marathwada, Akola, Wardha, and Amravati.",
+        "crop_link": "https://en.wikipedia.org/wiki/Cotton",
+        "crop_img": "/static/styles/images/cotton.jpeg",
+    },
+    "jute": {
+        "crop_dis": "In India Jute is cultivated on 0.71 million hectares with an annual production of 9.98 million bales. West Bengal, Assam and Bihar are the major jute growing states in the country, which accounts for about 98 percent of the country's jute area and production (State of Indian Agriculture, 2016-2017).",
+        "crop_link": "https://en.wikipedia.org/wiki/Jute",
+        "crop_img": "/static/styles/images/jute.jpeg",
+    },
+    "coffee": {
+        "crop_dis": "Coffee production in India is dominated in the hill tracts of South Indian states, with Karnataka accounting for 71%, followed by Kerala with 21% and Tamil Nadu (5% of overall production with 8,200 tonnes).",
+        "crop_link": "https://en.wikipedia.org/wiki/Coffee",
+        "crop_img": "/static/styles/images/cofee.jpeg",
+    },
+}
